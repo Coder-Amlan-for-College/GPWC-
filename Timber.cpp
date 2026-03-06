ecefa02cea7dff77925d5a73d41a51ded8f724a8
Coder-Amlan-for-College <nayakamlan188@gmail.com>
2026-03-06 17:17:03 +0530

    Add files via upload

--- Timber.cpp
+++ Timber.cpp
@@ -29,24 +29,26 @@ Sprite beeSprite1,beeSprite2,beeSprite3,beeSprite4;
 beeSprite1.setTexture(beeTexture);
 beeSprite1.setPosition(resolution.x/2.0+300/2.0+20,resolution.y/2);
 
-beeSprite2.setTexture(beeTexture);
-beeSprite2.setPosition(resolution.x/2.0-300/2.0,resolution.y/3);
+// beeSprite2.setTexture(beeTexture);
+// beeSprite2.setPosition(resolution.x/2.0-300/2.0,resolution.y/3);
 
-beeSprite3.setTexture(beeTexture);
-beeSprite3.setPosition(resolution.x/2.0,resolution.y/2.0);
+// beeSprite3.setTexture(beeTexture);
+// beeSprite3.setPosition(resolution.x/2.0,resolution.y/2.0);
 
-beeSprite4.setTexture(beeTexture);
-beeSprite4.setPosition(resolution.x/2.0-300/2.0-65,resolution.y/1.5);
+// beeSprite4.setTexture(beeTexture);
+// beeSprite4.setPosition(resolution.x/2.0-300/2.0-65,resolution.y/1.5);
+
+Clock clock;
+float beeSpeed=0.0f;
+float beeHeight=0.0f;
+bool beeActive=false;
 
 Texture cloudTexture;
 cloudTexture.loadFromFile("graphics/cloud.png");
 Sprite cloudSprite;
 cloudSprite.setTexture(cloudTexture);
 cloudSprite.setPosition(0,0);
-Clock deltaClock,clock;
-float beeSpeed = 300.0f;
-bool paused=false;
-Time dt;
+
 
 while(window.isOpen()){
     Event event;
@@ -58,23 +60,28 @@ while(window.isOpen()){
     if(Keyboard::isKeyPressed(Keyboard::Escape)){
         window.close();
     }
-    dt = deltaClock.restart();
-    if(!paused){
-        if(beeSprite1.getPosition().x<-100){
-            beeSprite1.setPosition(resolution.x,800);
+    Time dt = clock.restart();
+    if(!beeActive){
+        srand(time(0)*10);
+        beeSpeed=rand()%200+200;
+        srand(time(0)*20);
+        beeHeight=rand()%500+500;
+        beeSprite1.setPosition(2000,beeHeight);
+        beeActive=true;
     }
     else{
-        beeSprite1.setPosition(beeSprite1.getPosition().x-beeSpeed*dt.asSeconds(),800);
+        beeSprite1.setPosition(beeSprite1.getPosition().x-beeSpeed*dt.asSeconds(),beeSprite1.getPosition().y);
+        if(beeSprite1.getPosition().x < -100){
+            beeActive=false;
+        }
     }
-    }
-
     window.clear();
     window.draw(backgroundSprite);
     window.draw(treeSprite);
     window.draw(beeSprite1);
-    window.draw(beeSprite2);
-    window.draw(beeSprite3);
-    window.draw(beeSprite4);
+    // window.draw(beeSprite2);
+    // window.draw(beeSprite3);
+    // window.draw(beeSprite4);
     window.draw(cloudSprite);
     window.display();
 }
